2c4dab37a7bba8566e1aab421d787bb5626b4194
Muhammad Hanan <hanan.ppqita@gmail.com>
2023-08-30 10:31:29 +0700

    commit 3

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+//contoh simple sederhana
+
+/*
+ * req adalah singkatan dari request yang isi yang dikirimkan oleh oleh client
+ * contoh seperti body, primer, query
+ * res adalah singkatan dari respons yang isinya kita kirim ke client
+ * contoh seperti data, json, html, dan codeHTTP (default:200)
+ */
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Biar bisa running
+app.listen(3000, () => {
+  console.log("Hai Dev, Servermu udah jalan di localhost:3000");
+});
